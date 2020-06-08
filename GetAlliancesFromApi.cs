977b92131ebd75937f21e6881434781f6ec45d6c
SanjokLimbu <64683032+SanjokLimbu@users.noreply.github.com>
2020-06-08 21:08:23 +1000

    Add files via upload

---
+++ GetAlliancesFromApi.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using PoliticsAndWarAPIAccess.API.Implementation;
+using PoliticsAndWarAPIAccess.API.Models;
+using PWAPI.MyPWDatabaseTableAdapters;
+
+namespace PWAPI
+{
+    public class GetAlliancesFromApi
+    {
+        public static async Task GetAlliance()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["PWAPI"].ConnectionString;
+            try
+            {
+                using(SqlConnection con = new SqlConnection(connectionString))
+                {
+                    var data_Add = new SqlCommand("sp_add_data", con)
+                    {
+                        CommandType = CommandType.StoredProcedure
+                    };
+                    con.Open();
+                    data_Add.ExecuteNonQuery();
+                    con.Close();
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            
+
+        }
+    }
+}
