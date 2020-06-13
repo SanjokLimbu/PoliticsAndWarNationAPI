8299ac0e6765196295eff8799812ae25580d2f8f
SanjokLimbu <64683032+SanjokLimbu@users.noreply.github.com>
2020-06-13 14:10:29 +1000

    Add files via upload

---
+++ Interface/IAlliance.cs
@@ -0,0 +1,12 @@
+﻿using PWAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PWAPI.Interface
+{
+    public interface IAlliance
+    {
+        List<alliance> GetAlliances();
+    }
+}
---
+++ Interface/INation.cs
@@ -0,0 +1,13 @@
+﻿using PoliticsAndWarAPIAccess.API.Models;
+using PWAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PWAPI.Interface
+{
+    public interface INation
+    {
+        List<nation> GetAllNations();
+    }
+}
