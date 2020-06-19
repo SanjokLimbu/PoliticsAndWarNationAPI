ea62413a3cf991d5b0e1574ce590fe4007969aec
SanjokLimbu <64683032+SanjokLimbu@users.noreply.github.com>
2020-06-19 17:43:29 +1000

    Add files via upload
    Added pages for viewing nation of respective alliances.

---
+++ Pages/AlliancePage/AllianceDetails.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PWAPI.Interface;
+using PWAPI.Model;
+
+namespace MyWeb.Pages.AlliancePage
+{
+    public class AllianceDetailsModel : PageModel
+    {
+        private readonly INation nation;
+
+        public AllianceDetailsModel(INation nation)
+        {
+            this.nation = nation;
+        }
+        public IEnumerable<nation> MyNation { get; set; }
+        public void OnGet(int id)
+        {
+            MyNation = nation.GetAllianceUnits(id);
+        }
+    }
+}
