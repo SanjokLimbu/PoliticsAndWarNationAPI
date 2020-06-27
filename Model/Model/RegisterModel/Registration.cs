907c1936cd0efa9586b700a2030150c259c9fcc8
SanjokLimbu <64683032+SanjokLimbu@users.noreply.github.com>
2020-06-27 20:23:24 +1000

    Add files via upload

---
+++ Model/Model/RegisterModel/Registration.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PW.Model.RegisterModel
+{
+    public class Registration
+    {
+        [Key]
+        public int UserId { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage ="Password do not MATCH.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
