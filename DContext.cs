d7727ac6c9b3d9e31ab625730f9cff6721468959
SanjokLimbu <64683032+SanjokLimbu@users.noreply.github.com>
2020-06-27 20:25:47 +1000

    Add files via upload

---
+++ DContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PW.Model.RegisterModel;
+
+namespace PW
+{
+    public class DContext : IdentityDbContext
+    {
+        public DContext(DbContextOptions<DContext> options)
+            :base(options)
+        {    
+        }
+        public DbSet<Registration> Registrations { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
