67a4ff75362edff2b66351bd00e2a6f1d51caaf2
SanjokLimbu <64683032+SanjokLimbu@users.noreply.github.com>
2020-06-28 20:40:29 +1000

    Update AccountsController.cs

--- Controllers/AccountsController.cs
+++ Controllers/AccountsController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using PW.Model.RegisterModel;
+using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace PWWebApplication.Controllers
@@ -50,9 +52,14 @@ namespace PWWebApplication.Controllers
             return RedirectToPage("/Index");
         }
         [HttpGet]
-        public IActionResult Logins()
+        public async Task<IActionResult> Logins(string returnUrl)
         {
-            return View();
+            Login model = new Login
+            {
+                ReturnUrl = returnUrl,
+                ExternalLogins = (await signInManager.GetExternalAuthenticationSchemesAsync()).ToList()
+            };
+            return View(model);
         }
         [HttpPost]
         public async Task<IActionResult> Logins(Login login)
@@ -68,6 +75,63 @@ namespace PWWebApplication.Controllers
                 
             }
             return View(login);
+        }
+        [HttpPost]
+        public IActionResult ExternalLogin(string provider, string returnUrl)
+        {
+            var redirectUrl = Url.Action("ExternalLoginCallback", "Accounts",
+                new { ReturnUrl = returnUrl });
+            var properties = signInManager.ConfigureExternalAuthenticationProperties(provider, redirectUrl);
+            return new ChallengeResult(provider, properties);
+        }
+        public async Task<IActionResult>ExternalLoginCallback(string returnUrl = null, string remoteError = null)
+        {
+            returnUrl = returnUrl ?? Url.Content("~/");
+            Login login = new Login
+            {
+                ReturnUrl = returnUrl,
+                ExternalLogins = (await signInManager.GetExternalAuthenticationSchemesAsync()).ToList()
+            };
+            if(remoteError != null)
+            {
+                ModelState.AddModelError(string.Empty, $"Error from external provider: {remoteError}");
+                return View("Logins", login);
+            }
+            var info = await signInManager.GetExternalLoginInfoAsync();
+            if(info == null)
+            {
+                ModelState.AddModelError(string.Empty, "Error loading external login information.");
+                return View("Logins", login);
+            }
+            var signInResult = await signInManager.ExternalLoginSignInAsync(info.LoginProvider,
+                info.ProviderKey, isPersistent: false, bypassTwoFactor: true);
+            if (signInResult.Succeeded)
+            {
+                return LocalRedirect(returnUrl);
+            }
+            else
+            {
+                var email = info.Principal.FindFirstValue(ClaimTypes.Email);
+                if(email != null)
+                {
+                    var user = await userManager.FindByEmailAsync(email);
+                    if (user == null)
+                    {
+                        user = new IdentityUser
+                        {
+                            UserName = info.Principal.FindFirstValue(ClaimTypes.Email),
+                            Email = info.Principal.FindFirstValue(ClaimTypes.Email)
+                        };
+                        await userManager.CreateAsync(user);
+                    }
+                    await userManager.AddLoginAsync(user, info);
+                    await signInManager.SignInAsync(user, isPersistent: false);
+                    return LocalRedirect(returnUrl);
+                }
+                ViewBag.ErrorTitile = $"Email claim not received from: {info.LoginProvider}";
+                ViewBag.ErrorMessage = "Please register user";
+                return View("Error");
+            }
         }
     }
 }
